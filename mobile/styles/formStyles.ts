c675d8ce3ea4035f65a7141691cb6e8224bc1024
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-01-17 15:28:06 +0700

    card

--- mobile/styles/formStyles.ts
+++ mobile/styles/formStyles.ts
@@ -8,3 +8,14 @@ export const getFormStyles = (isDark: boolean) => StyleSheet.create({
     paddingBottom: 40,
     padding: 24,
   },
+
+  card: {
+    padding: 24,
+    borderRadius: 20,
+    elevation: 4,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    backgroundColor: isDark ? '#1E1E1E' : '#FFF',
+  },
