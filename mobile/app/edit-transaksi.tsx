3cfd481c398f72d588aae06517904a97f7af9c13
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-01-17 10:24:30 +0700

    stylesheet label

--- mobile/app/edit-transaksi.tsx
+++ mobile/app/edit-transaksi.tsx
@@ -160,4 +160,6 @@ const EditTransaksi = () => {
 const styles = StyleSheet.create({
     container: { flex: 1, padding: 16 },
     card: { padding: 20, borderRadius: 16, elevation: 4, shadowColor: '#000', shadowOpacity: 0.1, shadowRadius: 10 },
+    label: { fontSize: 14, fontWeight: '600', marginBottom: 8, marginTop: 15 },
+
 });
