d07e831d595e4bea4135423edfcf89b531336321
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-01-17 15:27:36 +0700

    fungsi getformstyles

--- mobile/styles/formStyles.ts
+++ mobile/styles/formStyles.ts
@@ -1,1 +1,10 @@
 import { StyleSheet } from 'react-native';
+
+export const getFormStyles = (isDark: boolean) => StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollContent: {
+    paddingBottom: 40,
+    padding: 24,
+  },
